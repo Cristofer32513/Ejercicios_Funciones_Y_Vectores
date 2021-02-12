3e1c23769034626c4eeb8e1bd4d6a13939ebbf14
Cristofer32513 <cristofer32513@gmali.com>
2021-02-12 10:27:16 -0600

    Funciones de llenado e impresion del vector completados.

--- src/DatosEstadisticos.scala
+++ src/DatosEstadisticos.scala
@@ -1,3 +1,29 @@
+import scala.io.StdIn.readLine
+
 object DatosEstadisticos {
 
+  def LlenarVector(vector:Array[Int]) : Array[Int] = {
+    for (i <- vector.indices) {
+      println("Ingrese el dato numero " + (i + 1) + "...")
+      vector(i) = readLine().toInt
+    }
+    vector
+  }
+
+  def ImprimirVector(vector:Array[Int]) : Unit = {
+    vector.foreach(println)
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    println("Ingrese la cantidad de datos a almacenar en el vector...")
+    var vectorDatos = new Array[Int](readLine().toInt)
+
+    vectorDatos = LlenarVector(vectorDatos)
+    ImprimirVector(vectorDatos)
+
+
+
+
+  }
 }
